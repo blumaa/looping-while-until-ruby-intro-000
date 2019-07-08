3566d44c8236f0283ee69ef4a5f2dee4cb6dda8b
Aaron Blum <blumaa@gmail.com>
2019-07-08 22:34:52 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,10 @@
 def using_while
   levitation_force = 6
-	loop do
+	
+  while levitation_force <11
+    puts "Wingardium Leviosa"
     levitation_force += 1
-	  while levitation_force <11
-      puts "Wingardium Leviosa"
-
 	  end
-	end
-  #your code here
 end
+
+
